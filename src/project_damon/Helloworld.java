5a3fd74597158419039ba81fe82e7fa29785b8d2
NEWRATEK <NEWRATEK@192.168.1.8>
2019-04-07 00:04:05 +0900

    initial commit

---
+++ src/project_damon/Helloworld.java
@@ -0,0 +1,9 @@
+package project_damon;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		System.out.println("test123 Newratek");
+	}
+
+}
