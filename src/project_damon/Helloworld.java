c89fa047334ad441a12a091716665b08f6efe654
NEWRATEK <NEWRATEK@192.168.1.8>
2019-04-07 00:14:57 +0900

    "addtional" message추가

--- src/project_damon/Helloworld.java
+++ src/project_damon/Helloworld.java
@@ -4,6 +4,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 		System.out.println("test123 Newratek");
+		System.out.println("additional1");
 	}
 
 }
